8329561797715dbaf7f19eeb49d42da13a7e76fe
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2024-03-04 19:53:48 +0000

    chapter 1-5 updates

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Hurricane, Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const hurricane = Hurricane({
+  subsets: ['latin'],
+  weight: '400',
+});
+
+export const lusitana = Lusitana({ weight: '400', subsets: ['latin'] });
